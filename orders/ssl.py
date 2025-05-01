97974c20183458edd00ade6e5da5d2d771f28e12
sahboddin <mdsahboddink@gmail.com>
2025-05-01 19:53:40 +0600

    Updated the project files

--- orders/ssl.py
+++ orders/ssl.py
@@ -65,9 +65,9 @@ def sslcommerz_payment_gateway(request, order_id, user_id, grand_total):
     post_body['total_amount'] = grand_total
     post_body['currency'] = "BDT"
     post_body['tran_id'] = unique_transaction_id_generator()
-    post_body['success_url'] = f"http://127.0.0.1:8000/order/success/{post_body['tran_id']}/{request.user.id}"
-    post_body['fail_url'] = 'http://127.0.0.1:8000/account/profile/'
-    post_body['cancel_url'] = 'http://127.0.0.1:8000/account/profile/'
+    post_body['success_url'] = f"https://vault-bazaar.onrender.com/order/success/{post_body['tran_id']}/{request.user.id}"
+    post_body['fail_url'] = 'https://vault-bazaar.onrender.com/account/profile/'
+    post_body['cancel_url'] = 'https://vault-bazaar.onrender.com/account/profile/'
     post_body['emi_option'] = 0
     post_body['cus_email'] = 'request.user.email'  # Retrieve email from the current user session
     post_body['cus_phone'] = 'request.user.phone'  # Retrieve phone from the current user session
